af30ff6f6680d4029ffa0c212d6414b750336f25
henriquelbk <loubackhenrique123@gmail.com>
2023-03-06 22:22:41 -0300

    ,

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ console.log('Bem vindo, Administrador')
 }
 
 permiteAdmin()
+
+for(let i = 1; i <= 10; i++){
+	console.log(`2 x ${i} = ${2*i}`)
+}
+
+const arrays = ['cds', 'oiwsh', ',', '4']
+
+for(i = 0; i <= 4; i++){
+    const maiuscula = arrays[i].toUpperCase()
+    console.log(maiuscula)
+}
